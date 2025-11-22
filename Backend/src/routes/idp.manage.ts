f661a1fcc3fdf31c7e1deed02141289f8e35729d
Harsha A K <harshaak2003@gmail.com>
2025-11-22 19:59:53 +0530

    somwthing changes

--- Backend/src/routes/idp.manage.ts
+++ Backend/src/routes/idp.manage.ts
@@ -220,8 +220,8 @@ router.get("/sso-login/:tenantCode", async (req, res) => {
   // ✅ Store HRM JWT in secure cookie
   res.cookie("token", hrmJwt, {
     httpOnly: true,
-    secure: true, // set true in production (HTTPS)
-    sameSite: "none",
+    secure: false, // set true in production (HTTPS)
+    sameSite: "lax",
     maxAge: 24 * 60 * 60 * 1000,
   });
   res.redirect(`http://localhost:8080/employee`);
